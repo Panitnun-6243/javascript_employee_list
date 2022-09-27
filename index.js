dd8e56f65a16ae54530bd1d0b3e2700fbc1f1f04
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-09-28 04:13:32 +0700

    Parse JSON to object and Loop to html element

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const loadTable = () => {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       console.log(this.responseText);
+      var trHTML = "";
+      const objects = JSON.parse(this.responseText);
+      for (let object of objects) {
+        trHTML += "<tr>";
+        trHTML += "<td>" + object["id"] + "</td>";
+        trHTML += "<td><img width='50px' class='avatar' src='" + object["avatar"] + "'></td>";
+        trHTML += "<td>" + object["fname"] + "</td>";
+        trHTML += "<td>" + object["lname"] + "</td>";
+        trHTML += "<td>" + object["username"] + "</td>";
+        trHTML += "<td></td>"//ปุ่ม edit/delete
+        trHTML += "</tr>";
+      }
+      document.getElementById("tableData").innerHTML = trHTML
     }
   };
 };
