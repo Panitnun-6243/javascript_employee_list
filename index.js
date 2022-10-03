0ef82b3d9ef1521cd0c7ab7ae8dcf3f566cacee1
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-04 05:01:20 +0700

    Create add new member modal dialog

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//GET request
 const loadTable = () => {
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", "https://www.melivecode.com/api/users");
@@ -10,15 +11,35 @@ const loadTable = () => {
       for (let object of objects) {
         trHTML += "<tr>";
         trHTML += "<td>" + object["id"] + "</td>";
-        trHTML += "<td><img width='50px' class='avatar' src='" + object["avatar"] + "'></td>";
+        trHTML +=
+          "<td><img width='50px' class='avatar' src='" +
+          object["avatar"] +
+          "'></td>";
         trHTML += "<td>" + object["fname"] + "</td>";
         trHTML += "<td>" + object["lname"] + "</td>";
         trHTML += "<td>" + object["username"] + "</td>";
-        trHTML += "<td><div style='margin-right:5px;display: flex;flex-direction: row;column-gap: 10px;'><button type='button' class='btn btn-outline-info'><i class='bi bi-trash text-light'></i> Edit</button><button type='button' class='btn btn-outline-warning'><i class='bi bi-trash text-light'></i> Delete</button></div></td>"
+        trHTML +=
+          "<td><div style='margin-right:5px;display: flex;flex-direction: row;column-gap: 10px;'><button type='button' class='btn btn-outline-info'><i class='bi bi-trash text-light'></i> Edit</button><button type='button' class='btn btn-outline-warning'><i class='bi bi-trash text-light'></i> Delete</button></div></td>";
         trHTML += "</tr>";
       }
-      document.getElementById("tableData").innerHTML = trHTML
+      document.getElementById("tableData").innerHTML = trHTML;
     }
   };
 };
 loadTable();
+//Add new member modal dialog
+const showUserCreateDialog = () => {
+  Swal.fire({
+    title: "Multiple inputs",
+    html:
+      '<input id="swal-input1" class="swal2-input">' +
+      '<input id="swal-input2" class="swal2-input">',
+    focusConfirm: false,
+    preConfirm: () => {
+      return [
+        document.getElementById("swal-input1").value,
+        document.getElementById("swal-input2").value,
+      ];
+    },
+  });
+};
