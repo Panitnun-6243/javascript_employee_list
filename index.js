ff9747f4e47d27fdbfb9180f065e8b431cdddfea
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-07 03:09:18 +0700

    Add POST method

--- index.js
+++ index.js
@@ -32,14 +32,40 @@ const showUserCreateDialog = () => {
   Swal.fire({
     title: "Multiple inputs",
     html:
-      '<input id="swal-input1" class="swal2-input">' +
-      '<input id="swal-input2" class="swal2-input">',
+      '<input id="fname" class="swal2-input" placeholder="First name">' +
+      '<input id="lname" class="swal2-input" placeholder="Last name">' +
+      '<input id="username" class="swal2-input" placeholder="Username">' +
+      '<input id="email" class="swal2-input" placeholder="Email">' +
+      '<input id="avatar" class="swal2-input" placeholder="Avatar link">',
     focusConfirm: false,
     preConfirm: () => {
-      return [
-        document.getElementById("swal-input1").value,
-        document.getElementById("swal-input2").value,
-      ];
+      userCreate()
     },
   });
 };
+
+const userCreate = () => {
+  const fname = document.getElementById("fname").value
+  const lname = document.getElementById("lname").value
+  const username = document.getElementById("username").value
+  const email = document.getElementById("email").value
+  const avatar = document.getElementById("avatar").value
+  const xhttp =  new XMLHttpRequest()
+  xhttp.open("POST", "https://www.melivecode.com/api/users/create")
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  xhttp.send(JSON.stringify({
+    "fname": fname,
+    "lname": lname,
+    "username": username,
+    "email": email,
+    "avatar": avatar
+  }))
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      console.log(this.responseText)
+      const object = JSON.parse(this.responseText);
+      Swal.fire(object['message'])
+      loadTable()
+    }
+  }
+}
