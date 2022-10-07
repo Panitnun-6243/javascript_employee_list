6be761618d6b6872626a6aada8b59a1fc39b4060
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-10-07 18:03:10 +0700

    Add PUT method

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const loadTable = () => {
   xhttp.send();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
       var trHTML = "";
       const objects = JSON.parse(this.responseText);
       for (let object of objects) {
@@ -19,7 +18,7 @@ const loadTable = () => {
         trHTML += "<td>" + object["lname"] + "</td>";
         trHTML += "<td>" + object["username"] + "</td>";
         trHTML +=
-          "<td><div style='margin-right:5px;display: flex;flex-direction: row;column-gap: 10px;'><button type='button' class='btn btn-outline-info'><i class='bi bi-trash text-light'></i> Edit</button><button type='button' class='btn btn-outline-warning'><i class='bi bi-trash text-light'></i> Delete</button></div></td>";
+          "<td><div style='margin-right:5px;display: flex;flex-direction: row;column-gap: 10px;'><button type='button' class='btn btn-outline-info' onclick='showUserUpdateDialog(" + object['id'] + ")'>Edit</button><button type='button' class='btn btn-outline-warning'>Delete</button></div></td>";
         trHTML += "</tr>";
       }
       document.getElementById("tableData").innerHTML = trHTML;
@@ -30,7 +29,7 @@ loadTable();
 //Add new member modal dialog
 const showUserCreateDialog = () => {
   Swal.fire({
-    title: "Multiple inputs",
+    title: "Add new employee",
     html:
       '<input id="fname" class="swal2-input" placeholder="First name">' +
       '<input id="lname" class="swal2-input" placeholder="Last name">' +
@@ -43,7 +42,7 @@ const showUserCreateDialog = () => {
     },
   });
 };
-
+//POST request
 const userCreate = () => {
   const fname = document.getElementById("fname").value
   const lname = document.getElementById("lname").value
@@ -62,10 +61,61 @@ const userCreate = () => {
   }))
   xhttp.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
-      console.log(this.responseText)
       const object = JSON.parse(this.responseText);
       Swal.fire(object['message'])
       loadTable()
     }
   }
 }
+//Update new member modal dialog
+const showUserUpdateDialog = (id) => {
+  const xhttp = new XMLHttpRequest()
+  xhttp.open("GET", "https://www.melivecode.com/api/users/" + id);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const object = JSON.parse(this.responseText);
+      const user = object['user']
+      Swal.fire({
+        title: `Edit employee info id: ${id}`,
+        html:
+          '<input id="id" type="hidden" class="swal2-input" placeholder="First name" value="'+ user['id'] +'">' +
+          '<input id="fname" class="swal2-input" placeholder="First name" value="'+ user['fname'] +'">' +
+          '<input id="lname" class="swal2-input" placeholder="Last name" value="'+ user['lname'] +'">' +
+          '<input id="username" class="swal2-input" placeholder="Username" value="'+ user['username'] +'">' +
+          '<input id="email" class="swal2-input" placeholder="Email" value="'+ user['email'] +'">' +
+          '<input id="avatar" class="swal2-input" placeholder="Avatar link" value="'+ user['avatar'] +'">',
+        focusConfirm: false,
+        preConfirm: () => {
+          userUpdate()
+        },
+      });
+    }}
+}
+//PUT request
+const userUpdate = () => {
+  const id = document.getElementById("id").value
+  const fname = document.getElementById("fname").value
+  const lname = document.getElementById("lname").value
+  const username = document.getElementById("username").value
+  const email = document.getElementById("email").value
+  const avatar = document.getElementById("avatar").value
+  const xhttp = new XMLHttpRequest()
+  xhttp.open("PUT", "https://www.melivecode.com/api/users/update")
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  xhttp.send(JSON.stringify({
+    "id": id,
+    "fname": fname,
+    "lname": lname,
+    "username": username,
+    "email": email,
+    "avatar": avatar
+  }))
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      const object = JSON.parse(this.responseText);
+      Swal.fire(object['message'])
+      loadTable()
+    }
+  }
+} 
